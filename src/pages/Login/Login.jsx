c43b6b195dc4b5a1b1df194e4b331dc0c0b5bc0c
abdulazizdevv <abdulazizkenjaoxunov@gmail.com>
2023-02-07 18:18:42 +0500

    changed

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -3,6 +3,8 @@ import { useContext, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
 import { UserContext } from "../../context/UserContext";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 export const Login = () => {
   const navigate = useNavigate();
@@ -11,14 +13,13 @@ export const Login = () => {
   const emailRef = useRef();
   const passwordRef = useRef();
 
-  const handleSubmit = (evt) => {
-    evt.preventDefault();
-    // useEffect(()=>{
+  const initialValues = {
+    email: "",
+    password: "",
+  };
+  const onSubmit = (values) => {
     axios
-      .post("http://localhost:8080/login", {
-        email: emailRef.current.value,
-        password: passwordRef.current.value,
-      })
+      .post("http://localhost:8080/login", values)
       .then((data) => {
         if (data.status === 200) {
           setToken(data.data.accessToken),
@@ -27,28 +28,60 @@ export const Login = () => {
         }
       })
       .catch((err) => console.log(err.message));
-    // },[])
   };
+
+  const validationSchema = Yup.object({
+    email: Yup.string().email("Invalid email format").required("Required!!!"),
+    password: Yup.string()
+      .min(3, "must be longer 3 characters")
+      .max(8, "must be last 8 characters")
+      .required("Required!!!"),
+  });
+
   return (
     <div className="shadow w-50 mx-auto p-5 mt-5">
       <h1 className="text-center">Login</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          ref={emailRef}
-          className="form-control my-3"
-          type="email"
-          placeholder="Email"
-        />
-        <input
-          ref={passwordRef}
-          className="form-control my-3"
-          type="password"
-          placeholder="Password"
-        />
-        <button className="btn btn-primary" type="submit">
-          SEND
-        </button>
-      </form>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={onSubmit}
+        validationSchema={validationSchema}
+      >
+        <Form>
+          <div className="mb-3">
+            <label htmlFor="email">Email</label>
+            <Field
+              id="email"
+              name="email"
+              className="form-control"
+              type="email"
+              placeholder="Email"
+            />
+            <ErrorMessage
+              name="email"
+              component={"span"}
+              className="text-danger"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password">Password </label>
+            <Field
+              id="password"
+              name="password"
+              className="form-control "
+              type="password"
+              placeholder="Password"
+            />
+            <ErrorMessage
+              name="password"
+              component={"span"}
+              className="text-danger"
+            />
+          </div>
+          <button className="btn btn-primary" type="submit">
+            SEND
+          </button>
+        </Form>
+      </Formik>
     </div>
   );
 };
